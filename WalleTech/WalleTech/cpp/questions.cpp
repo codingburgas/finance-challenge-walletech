9fd3326314ebbe92e05a71dc17b35e652d76ae32
LRRashkov22 <LRRashkov22@codingburgas.bg>
2024-11-04 01:56:05 +0200

    questions main function

---
+++ WalleTech/WalleTech/cpp/questions.cpp
@@ -0,0 +1,9 @@
+#include "../Include/Questions.h"
+
+void questions(Textures* t)
+{
+    flagQuestions(t); // flag handling function
+    QuestionOne(t); // question one function
+    QuestionTwo(t); // question two function
+    QuestionThreeFour(t); // questions three and four function
+}
