6db281074d7590b11d2e37d4c2f9779a9a06ec61
LRRashkov22 <LRRashkov22@codingburgas.bg>
2024-11-04 01:29:29 +0200

    input functions extern declare

---
+++ WalleTech/WalleTech/cpp/InputHandlerInitialize.cpp
@@ -0,0 +1,13 @@
+#include "../Include/InputHandler.h"
+
+bool flagUsername = 0, flagPassword = 0, flagEnter = 0, flagUsernameEntered = 0, flagNotRegistered = 0, flagIsRegistered = 0,flagCreateOne = 0, resultRegister = 0;
+clock_t startTime = clock();
+clock_t startTime2 = clock();
+clock_t startTime3 = clock();
+clock_t startTime4 = clock();
+clock_t startTime5 = clock();
+string username;
+string password;
+string starSet;
+bool flagFirstName = 0, flagLastName = 0, firstNameEntered = 0, flagLastNameEntered = 0, flagUsernameSignUp = 0, flagPasswordSignUp = 0;
+string firstName, lastName;
